38d4dad79f7e74eaafb2817c6d9c6d0aee5ef806
Aluon <paulacanoroman@gmail.com>
2023-09-19 12:12:55 +0200

    useEffect TaskLocal
    Utiliza el hook useEffect para cargar las tareas almacenadas en el localStorage cuando la aplicación se monta por primera vez. Si no hay tareas en el localStorage, el estado debe permanecer como un arreglo vacío.

--- local-storage-to-do-list/components/ToDoApp.jsx
+++ local-storage-to-do-list/components/ToDoApp.jsx
@@ -1,8 +1,14 @@
-import React, {useState} from 'react'
+import React, {useEffect, useState} from 'react'
 
 const ToDoApp = () => {
 
     const [taskList, setTaskList] = useState([])
+
+    useEffect(() => {
+      const taskLocal = JSON.parse(localStorage.getItem("listadoDeTareas"))
+      if (!taskLocal === null) setTaskList(taskLocal)
+    }, [])
+    
 
   return (
     <div>ToDoApp</div>
