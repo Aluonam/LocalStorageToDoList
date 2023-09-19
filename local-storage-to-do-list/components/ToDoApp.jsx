1617e71f767a81a5090abf409a0fd54bc1b914af
Aluon <paulacanoroman@gmail.com>
2023-09-19 12:09:00 +0200

    componente ToDoApp con useState

---
+++ local-storage-to-do-list/components/ToDoApp.jsx
@@ -0,0 +1,12 @@
+import React, {useState} from 'react'
+
+const ToDoApp = () => {
+
+    const [taskList, setTaskList] = useState([])
+
+  return (
+    <div>ToDoApp</div>
+  )
+}
+
+export default ToDoApp
