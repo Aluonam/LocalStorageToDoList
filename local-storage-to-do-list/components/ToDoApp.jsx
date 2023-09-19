10c018c68b1d080189820f79e84bcb2a5cbd2c9f
Aluon <paulacanoroman@gmail.com>
2023-09-19 12:23:12 +0200

    input

--- local-storage-to-do-list/components/ToDoApp.jsx
+++ local-storage-to-do-list/components/ToDoApp.jsx
@@ -3,15 +3,20 @@ import React, {useEffect, useState} from 'react'
 const ToDoApp = () => {
 
     const [taskList, setTaskList] = useState([])
+    const [inputUser, setInputUser] = useState("")
 
     useEffect(() => {
       const taskLocal = JSON.parse(localStorage.getItem("listadoDeTareas"))
       if (!taskLocal === null) setTaskList(taskLocal)
     }, [])
     
+   
 
   return (
-    <div>ToDoApp</div>
+    <>
+    <input type='text' onChange={(e)=>{setInputUser(e.target.value)}} value={inputUser}></input>
+    <button>Añadir tarea</button>
+    </>
   )
 }
 
